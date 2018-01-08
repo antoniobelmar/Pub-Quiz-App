1f8dfb90bb62f59166e91243955059557821daec
Allan Wazacz <cazwazacz@gmail.com>
2018-01-08 20:03:13 +0000

    Websocket Hack using native browser object

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 
 // const host = location.host;
 // console.log(host);
-const ws = new WebSocket(`ws://localhost:3000`);
+// const ws = new WebSocket(`ws://localhost:3000`);
 
-ws.onopen = function(){
+// function openSocket(num) {
+//   console.log('openSocket');
+//   ws.onopen = function(){
+//
+//     function sendMessage(num){
+//       var increment = parseInt(num)
+//       increment += 1
+//       ws.send(increment);
+//     };
+//     // setInterval(sendMessage, 2000);
+//
+//
+//
+//   };
+// }
 
-  function sendMessage(num){
-    var increment = parseInt(num)
-    increment += 1
-    ws.send(increment);
-  };
-  // setInterval(sendMessage, 2000);
-};
 
 class Test extends Component {
   constructor(props) {
@@ -20,9 +27,25 @@ class Test extends Component {
     this.state = {
       number: 0
     }
+    var that = this;
     this.activateLasers = this.activateLasers.bind(this);
     this.handleData = this.handleData.bind(this);
+    this.ws = new WebSocket(`ws://localhost:5000`);
+    const ws = this.ws;
+    ws.onopen = function() {
+      function sendMessage(){
+        var increment = parseInt(that.state.number);
+        increment += 1;
+        ws.send(increment);
+      };
+      setInterval(sendMessage, 1000);
+    }
+
+    ws.onmessage = function(event) {
+      that.setState({number: event.data});
+    }
   }
+
 
   activateLasers() {
     this.setState({number: this.state.number + 1});
@@ -36,7 +59,7 @@ class Test extends Component {
   render() {
     return (
       <div>
-        <button onClick={sendMessage(this.state.number)}>
+        <button>
           Activate Lasers
         </button>
         <h1>HELLO {this.state.number}</h1>
