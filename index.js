7a41f7b00fdfd5e282dbbb902e57a6e9b0483d16
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-08 15:39:15 +0000

    Setup websocket
    When connection is made to websocket and message is received, you send all the clients that message. Handled errors for when someone closes the connection.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
 
-app.use(express.static(__dirname + '/build'));
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
+
+// app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  res.sendFile('index.html')
-})
+  res.sendFile(__dirname + '/websockettest.html')
+});
 
-app.listen(5000, function(){
+wss.on('connection', function connection(ws, req){
+  const location = url.parse(req.url, true);
+
+  ws.on('message', function incoming(message){
+    wss.clients.forEach(function each(client){
+      if (client.readyState === WebSocket.OPEN){
+        client.send(message)
+      };
+    });
+  });
+
+  ws.on('error', function(error){
+    console.log('one person has left')
+  })
+});
+
+server.listen(5000, function(){
   console.log('server running on port 5000')
 })
