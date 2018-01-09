f53ad4f99684ef8a75146b972a63c2ce80be7cc2
antonio <antoniobelmardacosta@gmail.com>
2018-01-09 11:19:27 +0000

    Add conditional view folders to index.js depending on the environment

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/client/build"));
+} else {
+  app.use(express.static(__dirname + "/client/public"))
+}
+
 app.get('/', function(req, res){
   res.sendFile('index.html')
+})
+
+app.get('/hello', function(req, res){
+  res.send('hello')
 })
 
 app.listen(5000, function(){
