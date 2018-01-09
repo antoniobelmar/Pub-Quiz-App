a2986a0a408b99c349bd63b86c54cc83f1667dbc
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-09 12:20:41 +0000

    Create API end point which return a quiz

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const Quiz = require('./models/quiz')
 
-app.use(express.static(__dirname + '/build'));
-app.get('/', function(req, res){
-  res.sendFile('index.html')
+// app.use('/', express.static(__dirname + '/build'));
+
+app.get('/api/quiz/:id', function(req, res) {
+  Quiz.findById(req.params.id, function(err, quiz) {
+    if (err) {
+      console.log(err)
+    }else {
+      res.json(quiz)
+    }
+  })
 })
+
+// app.get('/', function(req, res){
+//   res.sendFile('index.html')
+// })
 
 app.listen(5000, function(){
   console.log('server running on port 5000')
