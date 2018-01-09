e366cf53de33a5cede32633d6bb806f75a70b3fa
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-01-09 16:44:40 +0000

    Remove redundant imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Heading from './components/Heading';
-import Test from './components/Test';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Heading />
-        <Test />
       </div>
     );
   }
