916ee1880e338a1ea6453234995d4c8c33ce1b30
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-09 12:38:11 +0000

    Show error if Quiz is not found

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 const Quiz = require('./models/quiz')
 
-// app.use('/', express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('/api/quiz/:id', function(req, res) {
   Quiz.findById(req.params.id, function(err, quiz) {
     if (err) {
-      console.log(err)
+      res.json({error: "Not Found"})
     }else {
       res.json(quiz)
     }
   })
 })
 
-// app.get('/', function(req, res){
-//   res.sendFile('index.html')
-// })
+app.get('/', function(req, res){
+  res.sendFile('index.html')
+})
 
 app.listen(5000, function(){
   console.log('server running on port 5000')
