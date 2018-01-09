abc65dd4d03020ad4af3c959d2fc0a662dcc8085
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-01-09 16:45:06 +0000

    Integrate web sockets into Quiz component

--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -4,15 +4,32 @@ import Question from './Question';
 class Quiz extends Component {
   constructor(props){
     super(props);
-    this.state = {name: 'Loading', questions: []}
-  }
+    this.state = { name: 'Loading', questions: [], number: 0 }
+  };
+
   componentDidMount(){
     let self = this;
+
     fetch('/api/quizzes/1')
-    .then((resp) => resp.json())
-    .then(function(data){
-      self.setState({name: data.name, questions: data.questions });
-    });
+      .then((resp) => resp.json())
+      .then(function(data){
+        self.setState({name: data.name, questions: data.questions });
+      });
+
+    let ws = new WebSocket('ws://localhost:5000');
+
+    ws.onopen = function() {
+      function sendMessage() {
+        ws.send(self.state.number + 1);
+      };
+      setInterval(sendMessage, 10000);
+    };
+
+    ws.onmessage = function(event) {
+      self.setState({ number: parseInt(event.data) });
+    };
+
+    this.setState({ ws: ws });
   };
 
   render() {
@@ -20,13 +37,25 @@ class Quiz extends Component {
         <div>
         <h1>{this.state.name}</h1>
         { this.state.questions.length > 0 &&
-          <Question question={this.state.questions[0]} />
+          <Question question={this.state.questions[this.state.number]} />
         }
         </div>
     )
   }
 }
 
-
-
 export default Quiz;
+
+// NOT CODE
+
+// class Test extends Component {
+//   constructor(props) {
+//     this.handleData = this.handleData.bind(this);
+//
+//
+//
+//   }
+//
+//   handleData(data) {
+//     console.log(data);
+//   }
