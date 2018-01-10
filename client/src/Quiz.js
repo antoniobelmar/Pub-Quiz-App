078ea8d469bb205fd1c9abe44ec5575ee6195e7a
antonio <antoniobelmardacosta@gmail.com>
2018-01-10 11:13:07 +0000

    Add ending page to the quiz revealing scores

--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -4,13 +4,13 @@ import Question from './Question';
 class Quiz extends Component {
   constructor(props){
     super(props);
-    this.state = { name: 'Loading', questions: [], number: 0 }
+    this.state = { name: 'Loading', questions: [], number: 0, score: 0 }
   };
 
   componentDidMount(){
     let self = this;
 
-    fetch('/api/quiz/5a55ad6fcc74d9ccf3705c5b')
+    fetch('/api/quiz/5a55e9fadc179c085c3c685a')
       .then((resp) => resp.json())
       .then(function(data){
         self.setState({name: data.name, questions: data.questions });
@@ -26,6 +26,12 @@ class Quiz extends Component {
     };
 
     ws.onmessage = function(event) {
+      var radios = document.getElementsByName('options')
+      radios.forEach(function(option) {
+        if(option.checked === true && option.value === self.state.questions[self.state.number].answer[0]) {
+          self.state.score += 1
+        }
+      })
       self.setState({ number: parseInt(event.data) });
     };
 
@@ -38,6 +44,12 @@ class Quiz extends Component {
         <h1>{this.state.name}</h1>
         { this.state.questions.length > 0 && this.state.number < this.state.questions.length &&
           <Question question={this.state.questions[this.state.number]} />
+        }
+        { this.state.number >= this.state.questions.length &&
+          <div>
+            <h2> Thanks for playing! </h2>
+            <h3> Your score was {this.state.score} </h3>
+          </div>
         }
         </div>
     )
