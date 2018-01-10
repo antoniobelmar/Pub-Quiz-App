5f2cd14ad62b1e094f74c4f8105c3f4c81963917
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-10 12:16:07 +0000

    Add ToggleDisplay feature in Quiz.js
    Imported ToggleDisplay in Quiz.js.
    Created new button 'Start QUIZ!' and through toggle display
    showed the quizzes and hided the button tag through a onClick attribute.

--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import Question from './Question';
+import ToggleDisplay from 'react-toggle-display';
 
 class Quiz extends Component {
   constructor(props){
     super(props);
-    this.state = { name: 'Loading', questions: [], number: 0, score: 0 }
+    this.state = { name: 'Loading', questions: [], number: 0, score: 0, show: false }
   };
 
   componentDidMount(){
     let self = this;
 
-    fetch('/api/quiz/5a55e9fadc179c085c3c685a')
+    fetch('/api/quiz/5a5602c29e64c01785ddd8b8')
       .then((resp) => resp.json())
       .then(function(data){
         self.setState({name: data.name, questions: data.questions });
@@ -38,19 +39,32 @@ class Quiz extends Component {
     this.setState({ ws: ws });
   };
 
+  hideButtonShowQuiz() {
+    this.setState({show: true})
+  }
+
   render() {
+
       return(
         <div>
+
+        <ToggleDisplay if={!this.state.show}>
+        <button type="button" onClick={ () => this.hideButtonShowQuiz() } >Start QUIZ!</button>
+        </ToggleDisplay>
+
+        <ToggleDisplay show={this.state.show}>
         <h1>{this.state.name}</h1>
         { this.state.questions.length > 0 && this.state.number < this.state.questions.length &&
           <Question question={this.state.questions[this.state.number]} />
         }
+          {console.log(this.state.questions)}
         { this.state.number >= this.state.questions.length &&
           <div>
             <h2> Thanks for playing! </h2>
             <h3> Your score was {this.state.score} </h3>
           </div>
         }
+        </ToggleDisplay>
         </div>
     )
   }
