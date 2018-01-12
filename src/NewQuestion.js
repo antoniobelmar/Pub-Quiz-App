c308438af8fccc3e36790ef24f850891c1ff7555
antonio <antoniobelmardacosta@gmail.com>
2018-01-12 15:10:35 +0000

    Add question component to render questions on the page

--- src/NewQuestion.js
+++ src/NewQuestion.js
@@ -3,10 +3,32 @@ import React, { Component } from 'react';
 class NewQuestion extends Component {
   render(){
     return(
-      <p>Add a question</p>
-
+      <div style={localStyles.questionRow}>
+        <div>
+          {'Q' + (this.props.index + 1)}
+        </div>
+      <input
+        type="text"
+        placeholder={this.props.question.placeholder}
+        value={this.props.question.text}
+        onChange={(event)=>this.props.handleChangeQuestion(this.props.index, event)} >
+      </input>
+      <div
+        onClick={this.props.addQuestion}
+        >
+          +
+      </div>
+    </div>
     );
   };
 };
+
+const localStyles = {
+  questionRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '5px',
+  }
+}
 
 export default NewQuestion;
