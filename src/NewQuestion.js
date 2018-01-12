cb486bb65a58fca2ebbbe9674bfd0b29ecdc0189
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-12 12:24:56 +0000

    Create new question component

---
+++ src/NewQuestion.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewQuestion extends Component {
+  render(){
+    return(
+      <p>Add a question</p>
+
+    );
+  };
+};
+
+export default NewQuestion;
