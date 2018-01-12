b12bad731c1e670c6bed20c18b06d32d62c3f316
Allan Wazacz <cazwazacz@gmail.com>
2018-01-12 12:08:11 +0000

    Create other page for reference

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home';
 import Quiz from './Quiz';
+import OtherPage from './OtherPage';
 
 const Routes = () => (
   <Router>
-    <Route path="/quiz/:quizId" component={Quiz}/>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/quiz/:quizId" component={Quiz}/>
+      <Route path="/otherPage" component={OtherPage}/>
+    </div>
   </Router>
 )
 
