d55e5eadf6d71c9fabfb2b27e4bd1eeceecd9e19
Allan Wazacz <cazwazacz@gmail.com>
2018-01-12 11:49:20 +0000

    Render quiz at /quiz path

--- src/Routes.js
+++ src/Routes.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Quiz from './Quiz';
+
+const Routes = () => (
+  <Router>
+    <Route path="/quiz" component={Quiz}/>
+  </Router>
+)
+
+export default Routes;
