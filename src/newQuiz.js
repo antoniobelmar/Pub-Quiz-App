7864f96385bf0d517d7b6aa0fb8d07b150146f89
antonio <antoniobelmardacosta@gmail.com>
2018-01-12 13:00:01 +0000

    Store quiz name as state of the new quiz component

--- src/newQuiz.js
+++ src/newQuiz.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import NewQuestion from './NewQuestion' ;
 
 class NewQuiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', questions: [{type: '', text: '', options: [{text: ''}], correctAnswer: [{text: ''}] }]}
+
+    this.handleChangeName = this.handleChangeName.bind(this)
+  }
+
+  handleChangeName(event) {
+    this.setState({name: event.target.value})
+  }
+
   render() {
+    {console.log(this.state)}
     return(
       <div>
       <p>Quiz Name</p>
-      <input placeholder='Type your quiz name'></input>
+      <input type="text" placeholder="Type your quiz name" value={this.state.name} onChange={this.handleChangeName} ></input>
       </div>
     );
   };
