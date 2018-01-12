8f806831be9ef05db755d51f3c007e82a6ffedc3
Allan Wazacz <cazwazacz@gmail.com>
2018-01-12 12:16:18 +0000

    Add link to quiz

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Home = () => (
-  <h1>Home page</h1>
+  <div>
+    <h1>Home page</h1>
+    <a href="/quiz/5a56302e0a8cc10014501d8f">A quiz</a>
+  </div>
 )
 
 export default Home;
