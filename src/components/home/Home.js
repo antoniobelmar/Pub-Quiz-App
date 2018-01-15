1f181c61dd84901d623c396fb2ec5a738ea34631
antonio <antoniobelmardacosta@gmail.com>
2018-01-15 15:22:01 +0000

    Update Home component to render QuizList component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import QuizList from './QuizList'
 
 class Home extends Component {
   constructor(props){
     super(props);
-    this.state = { quizLinks: '' };
+    this.state = { quizzes: [] };
   }
 
   componentDidMount() {
@@ -12,11 +13,11 @@ class Home extends Component {
     axios.get('http://pub-quiz-api.herokuapp.com/quiz')
     // axios.get('http://localhost:5000/quiz')
       .then(function(response) {
-        let array = [];
+        let state = self.state
         response.data.forEach(function(quiz, key) {
-          array.push({key: key, id: quiz._id, name: quiz.name});
+          state.quizzes.push({id: quiz._id, name: quiz.name});
         })
-        self.setState({ quizLinks: array })
+        self.setState({state})
       })
       .catch(function(error) {
         console.log(error);
@@ -28,6 +29,11 @@ class Home extends Component {
       <div>
         <h1>Home page</h1>
         <a href="/quiz/new"><button>Create a quiz!</button></a>
+        {this.state.quizzes.length > 0 &&
+          <QuizList
+            quizzes = {this.state.quizzes}
+          />
+        }
       </div>
     )
   }
