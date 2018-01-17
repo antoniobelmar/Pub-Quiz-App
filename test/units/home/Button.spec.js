4b0ea99515b8604b24352381382392bb7067e604
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-17 17:49:53 +0000

    Delete <div> test for Button component + update callback test

--- test/units/home/Button.spec.js
+++ test/units/home/Button.spec.js
@@ -4,15 +4,11 @@ import Button from '../../../src/components/quizbuilder/Button';
 describe('Button item', () => {
   const wrapper = shallow(<Button />)
 
-  it('should be a div item', () => {
-    expect(wrapper.type()).to.eql('div');
-  });
-
   it('when the button tag is clicked a callback function is run', () => {
     let fakeCallBack = spy()
-    wrapper.setProps({ callback: fakeCallBack})
+    wrapper.setProps({ callback: fakeCallBack, arg1: "", arg2: ""})
     wrapper.find('#general-quiz-button').simulate('click')
-    expect(fakeCallBack.calledOnce).to.equal(true);
+    expect(fakeCallBack.calledWith("", "")).to.equal(true);
   });
 
 });
